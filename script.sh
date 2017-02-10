bca95598aab8fd91d7015f3489fa10af6d9f8889
Gabriele Giannini <gabrielegiannini2@gmail.com>
2017-02-10 20:29:20 +0100

    asd8

--- script.sh
+++ script.sh
@@ -1,9 +1,4 @@
 #/bin/bash
-NEW_DISPLAY=42
-
-echo "Using first available display :${NEW_DISPLAY}"
-
-OLD_DISPLAY=${DISPLAY}
 bash script2.sh
-vncserver ":${NEW_DISPLAY}" -localhost -geometry 1600x1200 -depth 16
+vncserver
 export DISPLAY=:0.0
