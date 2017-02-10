2d75773e9524594d078cbf42c576c88546bc2eee
Gabriele Giannini <gabrielegiannini2@gmail.com>
2017-02-10 20:20:42 +0100

    asd6

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ echo "Using first available display :${NEW_DISPLAY}"
 
 OLD_DISPLAY=${DISPLAY}
 bash script2.sh
-vncserver-x11 ":${NEW_DISPLAY}" -localhost -geometry 1600x1200 -depth 16
+vncserver ":${NEW_DISPLAY}" -localhost -geometry 1600x1200 -depth 16
 export DISPLAY=:${NEW_DISPLAY}
