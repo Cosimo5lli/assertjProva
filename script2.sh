799c1d88a90bd5258640b65aeba62db8ae690983
Gabriele Giannini <gabrielegiannini2@gmail.com>
2017-02-10 19:22:28 +0100

    asd

--- script2.sh
+++ script2.sh
@@ -11,6 +11,10 @@ expect "Verify:"
 send "$mypass\r"
 expect "Would you like to enter a view-only password (y/n)?"
 send "n"
+expect "Password:"
+send "$mypass\r"
+expect "Verify:"
+send "$mypass\r"
 expect eof
 exit
 EOF
